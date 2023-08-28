3f6423f88ff04a7bc6120bc677872b6966d85493
osparking <jbpark03@gmail.com>
2023-08-28 14:32:25 +0900

    using utf-8 for bean identifier

--- src/main/java/ch02_ex3/config/ProjectConfig.java
+++ src/main/java/ch02_ex3/config/ProjectConfig.java
@@ -7,21 +7,21 @@ import ch02_ex3.entity.Parrot;
 
 @Configuration
 public class ProjectConfig {
-	@Bean
+	@Bean("루키")
 	Parrot parrot1() {
 		var p = new Parrot();
 		p.setName("루키");
 		return p;
 	}
 
-	@Bean
+	@Bean(name="미키")
 	Parrot parrot2() {
 		var p = new Parrot();
 		p.setName("미키");
 		return p;
 	}
 
-	@Bean
+	@Bean(value="리키")
 	Parrot parrot3() {
 		var p = new Parrot();
 		p.setName("리키");
