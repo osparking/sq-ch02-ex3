4b1b7d6b4895b8b5fad03c75115ca2f6d2dc0bc3
osparking <jbpark03@gmail.com>
2023-08-28 14:09:54 +0900

    multiple beans can't be read using class type

--- src/main/java/ch02_ex3/main/Main.java
+++ src/main/java/ch02_ex3/main/Main.java
@@ -1,14 +1,22 @@
 package ch02_ex3.main;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.PrintStream;
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import ch02_ex3.config.ProjectConfig;
+import ch02_ex3.entity.Parrot;
 
 public class Main {
 
 	public static void main(String[] args) {
 		var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		var ps = new PrintStream(System.out, true, UTF_8);
+		var p = (Parrot) context.getBean(Parrot.class);
 		
+		ps.println(p.getName());
 	}
 
 }
