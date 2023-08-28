82a01b63ee8ee843e2a95ea684c7ad37d7d758bb
osparking <jbpark03@gmail.com>
2023-08-28 12:39:03 +0900

    name attribute, 3 parrot beans added

--- src/main/java/ch02_ex3/config/ProjectConfig.java
+++ src/main/java/ch02_ex3/config/ProjectConfig.java
@@ -1,8 +1,31 @@
 package ch02_ex3.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import ch02_ex3.entity.Parrot;
 
 @Configuration
 public class ProjectConfig {
+	@Bean
+	Parrot parrot1() {
+		var p = new Parrot();
+		p.setName("루키");
+		return p;
+	}
+
+	@Bean
+	Parrot parrot2() {
+		var p = new Parrot();
+		p.setName("미키");
+		return p;
+	}
+
+	@Bean
+	Parrot parrot3() {
+		var p = new Parrot();
+		p.setName("리키");
+		return p;
+	}
 
 }
